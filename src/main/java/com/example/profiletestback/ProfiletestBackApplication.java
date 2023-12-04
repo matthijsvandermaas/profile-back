882bdef4d2e6e9115b937febe817ba7b52cb3146
matthijsvandermaas <matthijsvandermaas@yahoo.com>
2023-12-04 18:32:14 +0100

    first commit

---
+++ src/main/java/com/example/profiletestback/ProfiletestBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.profiletestback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfiletestBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfiletestBackApplication.class, args);
+    }
+
+}
